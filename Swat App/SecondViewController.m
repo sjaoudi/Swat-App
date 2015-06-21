5772de8b3048ff6fe3e51e12043b83762e707c44
sjaoudi17 <sjaoudi17@gmail.com>
2015-06-21 00:52:57 -0400

    Parser grabs titles, still have to implement regexes for description, and page requests for additional data

--- Swat App/SecondViewController.m
+++ Swat App/SecondViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SecondViewController.h"
+#import "MWFeedParser.h"
 
 @interface SecondViewController ()
 
@@ -15,12 +16,17 @@
 @implementation SecondViewController
 
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    NSURL *rssURL =[[NSURL alloc] initWithString:@"http://calendar.swarthmore.edu/calendar/RSSSyndicator.aspx?category=&location=&type=N&starting=5/1/2015&ending=5/29/2015&binary=Y&keywords=&ics=Y"];
+    NSURL *rssURL =[[NSURL alloc] initWithString:@"http://calendar.swarthmore.edu/calendar/RSSSyndicator.aspx?category=&location=&type=N&starting=5/1/2015&ending=5/15/2015&binary=Y&keywords=&ics=Y"];
+    
+    
     NSXMLParser *parser = [[NSXMLParser alloc] initWithContentsOfURL:rssURL];
+    //NSXMLParser *parser = [[NSXMLParser alloc] initWithData:xmlData];
+    
     [parser setDelegate:self];
     [parser parse];
 }
@@ -30,20 +36,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+// Finds all titles.
+- (void)parser:(NSXMLParser *)parser foundCDATA:(NSData *)CDATABlock
+{
+    NSString *someString = [[NSString alloc] initWithData:CDATABlock encoding:NSUTF8StringEncoding];
+    NSLog(@"Found CDATA: %@", someString);
+}
+
+
 -(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict
 {
     self.currentElement = elementName;
-    if([self.currentElement isEqualToString:@"title"])
+    if([self.currentElement isEqualToString:@"pubDate"])
     {
-        self.currentTitle = [NSMutableString string];
+        //self.currentTitle = [NSMutableString string];
     }
 }
 
+
 -(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
 {
-    if([self.currentElement isEqualToString:@"title"])
+    if([self.currentElement isEqualToString:@"pubDate"])
     {
-        NSLog(@"%@", self.currentTitle);
+        //NSLog(@"%@", self.currentTitle);
     }
     self.currentTitle=nil;
     self.currentElement=nil;
@@ -52,10 +68,28 @@
 -(void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string
 {
     if (!self.currentElement) return;
-    if([self.currentElement isEqualToString:@"title"])
+    
+    if([self.currentElement isEqualToString:@"pubDate"])
+    //if([self.currentTitle hasPrefix:@"<![CDATA"])
     {
         self.currentTitle=string;
+        NSLog(@"Characters: %@", string);
     }
+    
 }
+
+//- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string{
+//    
+//    if(!self.currentElement)
+//        self.currentElement = [[NSMutableString alloc] initWithString:string];
+//    else
+//        self.currentTitle=string;
+//    if ([self.currentElement isEqualToString:@"title"]) {
+//        NSLog(@"Characters: %@", self.currentTitle);
+//    }
+//
+//}
+
+
 
 @end
