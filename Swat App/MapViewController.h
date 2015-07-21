a64934771d2df9321eb846f9b3125c43243c4916
sjaoudi17 <sjaoudi17@gmail.com>
2015-07-21 00:16:31 -0400

    third tab button added for future map view

---
+++ Swat App/MapViewController.h
@@ -0,0 +1,21 @@
+//
+//  MapViewController.h
+//  Swat App
+//
+//  Created by Steve Jaoudi on 7/20/15.
+//  Copyright (c) 2015 Steve Jaoudi. All rights reserved.
+//
+
+#ifndef Swat_App_MapViewController_h
+#define Swat_App_MapViewController_h
+
+
+#endif
+
+#import <UIKit/UIKit.h>
+#import <MBXMapKit/MBXMapKit.h>
+
+@interface MapViewController : UIViewController {
+    
+}
+@end
---
+++ Swat App/MapViewController.m
@@ -0,0 +1,10 @@
+//
+//  MapViewController.m
+//  Swat App
+//
+//  Created by Steve Jaoudi on 7/20/15.
+//  Copyright (c) 2015 Steve Jaoudi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MBXMapKit/MBXMapKit.h>
