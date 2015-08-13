aa371a12a8f6b151ea73e9cdde08f49ce83d59f8
sjaoudi17 <sjaoudi17@gmail.com>
2015-08-12 22:34:11 -0400

    added about view to be filled

---
+++ Swat App/AboutViewController.h
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.h
+//  Swat App
+//
+//  Created by Steve Jaoudi on 8/12/15.
+//  Copyright (c) 2015 Steve Jaoudi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ Swat App/AboutViewController.m
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.m
+//  Swat App
+//
+//  Created by Steve Jaoudi on 8/12/15.
+//  Copyright (c) 2015 Steve Jaoudi. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@implementation AboutViewController
+
+@end
