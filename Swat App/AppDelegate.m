ae092285a8c472a8fb99aa9f25a961070f05652b
sjaoudi17 <sjaoudi17@gmail.com>
2015-08-24 23:16:26 -0400

    submitted version 1.0

--- Swat App/AppDelegate.m
+++ Swat App/AppDelegate.m
@@ -38,8 +38,8 @@
 }
 
 - (void)parseData {
-    NSURL *dashURL = [NSURL URLWithString:@"https://secure.swarthmore.edu/dash/"];
-    //NSURL *dashURL = [NSURL URLWithString:@"http://web.archive.org/web/20121004221810/https://secure.swarthmore.edu/dash/"];
+    //NSURL *dashURL = [NSURL URLWithString:@"https://secure.swarthmore.edu/dash/"];
+    NSURL *dashURL = [NSURL URLWithString:@"http://web.archive.org/web/20121004221810/https://secure.swarthmore.edu/dash/"];
     NSData *dashData = [NSData dataWithContentsOfURL:dashURL];
     NSString *dashString = [[NSString alloc] initWithData:dashData encoding:NSUTF8StringEncoding];
     
